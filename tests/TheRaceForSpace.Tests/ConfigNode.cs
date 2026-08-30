8a409ecb9e4c29d39413c3a602ddfaf615199e06
bdwindowservices-cloud <bdwindowservices@gmail.com>
2026-08-30 22:27:11 +0100

    Support child nodes in persistence tests

--- tests/TheRaceForSpace.Tests/ConfigNode.cs
+++ tests/TheRaceForSpace.Tests/ConfigNode.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 public sealed class ConfigNode
 {
     private readonly Dictionary<string, string> _values = new Dictionary<string, string>();
+    private readonly Dictionary<string, List<ConfigNode>> _nodes =
+        new Dictionary<string, List<ConfigNode>>();
 
     public void AddValue(string name, object value)
     {
@@ -16,5 +18,31 @@ public sealed class ConfigNode
     {
         string value;
         return _values.TryGetValue(name, out value) ? value : null;
+    }
+
+    public ConfigNode AddNode(string name)
+    {
+        List<ConfigNode> nodes;
+        if (!_nodes.TryGetValue(name, out nodes))
+        {
+            nodes = new List<ConfigNode>();
+            _nodes[name] = nodes;
+        }
+
+        var node = new ConfigNode();
+        nodes.Add(node);
+        return node;
+    }
+
+    public ConfigNode GetNode(string name)
+    {
+        List<ConfigNode> nodes;
+        return _nodes.TryGetValue(name, out nodes) && nodes.Count > 0 ? nodes[0] : null;
+    }
+
+    public ConfigNode[] GetNodes(string name)
+    {
+        List<ConfigNode> nodes;
+        return _nodes.TryGetValue(name, out nodes) ? nodes.ToArray() : new ConfigNode[0];
     }
 }

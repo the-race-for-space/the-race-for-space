91933327cde807e37dc47e5ee846ef27fb403600
bdwindowservices-cloud <bdwindowservices@gmail.com>
2026-08-30 22:10:08 +0100

    Add collection-driven rival simulation tests

---
+++ tests/TheRaceForSpace.Tests/Simulation/RivalSimulationCollectionTests.cs
@@ -0,0 +1,231 @@
+using System.Collections.Generic;
+using TheRaceForSpace.Funding;
+using TheRaceForSpace.Milestones;
+using TheRaceForSpace.Programs;
+using TheRaceForSpace.Simulation;
+
+namespace TheRaceForSpace.Tests.Simulation
+{
+    internal static class RivalSimulationCollectionTests
+    {
+        public static void SelectsOnlyAvailableAchievementFromCollection()
+        {
+            var player = new SpaceProgramState("Player", true);
+            player.RecordAchievement(PrototypeMilestones.CrewedOrbitId, 1.0);
+            var aster = new SpaceProgramState("Aster", false);
+            var cobalt = new SpaceProgramState("Cobalt", false);
+
+            MilestoneDefinition milestone = PrototypeMilestones.FindById(
+                PrototypeMilestones.MinmusCrewedOrbitId);
+            var achievementProgrammes = new List<AchievementFundingProgramme>
+            {
+                new AchievementFundingProgramme(
+                    milestone.Id,
+                    milestone.Name,
+                    milestone.ObjectiveDescription,
+                    300000.0,
+                    "Display text",
+                    milestone.PrerequisiteMilestoneId)
+            };
+
+            RivalSimulation.Refresh(
+                player,
+                aster,
+                cobalt,
+                0.0,
+                achievementProgrammes,
+                new List<FundingProgramme>());
+
+            TestAssert.Equal(PrototypeMilestones.MinmusCrewedOrbitId, aster.NextMissionTargetId);
+            TestAssert.Equal("Minmus Crewed Orbit", aster.NextLaunchBodyName);
+        }
+
+        public static void LockedAchievementIsExcludedFromCollection()
+        {
+            var player = new SpaceProgramState("Player", true);
+            var aster = new SpaceProgramState("Aster", false);
+            var cobalt = new SpaceProgramState("Cobalt", false);
+
+            MilestoneDefinition milestone = PrototypeMilestones.FindById(
+                PrototypeMilestones.MunProbeOrbitId);
+            var achievementProgrammes = new List<AchievementFundingProgramme>
+            {
+                new AchievementFundingProgramme(
+                    milestone.Id,
+                    milestone.Name,
+                    milestone.ObjectiveDescription,
+                    200000.0,
+                    "Display text",
+                    milestone.PrerequisiteMilestoneId)
+            };
+
+            RivalSimulation.Refresh(
+                player,
+                aster,
+                cobalt,
+                0.0,
+                achievementProgrammes,
+                new List<FundingProgramme>());
+
+            TestAssert.Equal(null, aster.NextMissionTargetId);
+        }
+
+        public static void SatelliteProgrammeRemainsRepeatable()
+        {
+            var player = new SpaceProgramState("Player", true);
+            var aster = new SpaceProgramState("Aster", false);
+            var cobalt = new SpaceProgramState("Cobalt", false);
+            aster.SetSatelliteCount("Duna", 3);
+
+            var fundingProgrammes = new List<FundingProgramme>
+            {
+                new FundingProgramme(
+                    "duna-network",
+                    "Duna Orbital Network",
+                    "Duna",
+                    5,
+                    100000.0,
+                    true,
+                    null,
+                    null)
+            };
+
+            RivalSimulation.Refresh(
+                player,
+                aster,
+                cobalt,
+                0.0,
+                new List<AchievementFundingProgramme>(),
+                fundingProgrammes);
+
+            TestAssert.Equal("duna-network", aster.NextMissionTargetId);
+            TestAssert.Equal("Duna", aster.NextLaunchBodyName);
+        }
+
+        public static void CompletesArbitrarySatelliteProgramme()
+        {
+            var player = new SpaceProgramState("Player", true);
+            var aster = new SpaceProgramState("Aster", false)
+            {
+                NextMissionTargetId = "duna-network",
+                LaunchProgressPercent = 100
+            };
+            var cobalt = new SpaceProgramState("Cobalt", false);
+
+            var fundingProgrammes = new List<FundingProgramme>
+            {
+                new FundingProgramme(
+                    "duna-network",
+                    "Duna Orbital Network",
+                    "Duna",
+                    5,
+                    100000.0,
+                    true,
+                    null,
+                    null)
+            };
+
+            RivalSimulation.Refresh(
+                player,
+                aster,
+                cobalt,
+                1234.0,
+                new List<AchievementFundingProgramme>(),
+                fundingProgrammes);
+
+            TestAssert.Equal(1, aster.GetSatelliteCount("Duna"));
+            TestAssert.Equal(0, aster.LaunchProgressPercent);
+            TestAssert.Equal("duna-network", aster.NextMissionTargetId);
+        }
+
+        public static void CollectionCostUsesProgrammeBody()
+        {
+            var aster = new SpaceProgramState("Aster", false)
+            {
+                NextMissionTargetId = "duna-network"
+            };
+            var fundingProgrammes = new List<FundingProgramme>
+            {
+                new FundingProgramme(
+                    "duna-network",
+                    "Duna Orbital Network",
+                    "Duna",
+                    5,
+                    100000.0,
+                    true,
+                    null,
+                    null)
+            };
+
+            double cost = RivalSimulation.CalculateLaunchProgressCost(
+                aster,
+                new List<AchievementFundingProgramme>(),
+                fundingProgrammes);
+
+            TestAssert.Equal(40000.0, cost);
+        }
+
+        public static void AchievementCompletionUsesMilestoneDefinition()
+        {
+            const double completionUniversalTime = 4321.0;
+            var player = new SpaceProgramState("Player", true);
+            player.RecordAchievement(PrototypeMilestones.ProbeOrbitId, 1.0);
+            var aster = new SpaceProgramState("Aster", false)
+            {
+                NextMissionTargetId = PrototypeMilestones.MunProbeOrbitId,
+                LaunchProgressPercent = 100
+            };
+            var cobalt = new SpaceProgramState("Cobalt", false);
+
+            MilestoneDefinition milestone = PrototypeMilestones.FindById(
+                PrototypeMilestones.MunProbeOrbitId);
+            var achievementProgrammes = new List<AchievementFundingProgramme>
+            {
+                new AchievementFundingProgramme(
+                    milestone.Id,
+                    milestone.Name,
+                    milestone.ObjectiveDescription,
+                    200000.0,
+                    "Display text",
+                    milestone.PrerequisiteMilestoneId)
+            };
+
+            RivalSimulation.Refresh(
+                player,
+                aster,
+                cobalt,
+                completionUniversalTime,
+                achievementProgrammes,
+                new List<FundingProgramme>());
+
+            TestAssert.True(
+                aster.HasAchievement(PrototypeMilestones.MunProbeOrbitId),
+                "Achievement completion should record the milestone ID.");
+            TestAssert.Equal(
+                completionUniversalTime,
+                aster.GetAchievementUniversalTime(PrototypeMilestones.MunProbeOrbitId));
+            TestAssert.Equal(1, aster.GetSatelliteCount("Mun"));
+            TestAssert.Equal(null, aster.NextMissionTargetId);
+        }
+    }
+
+    internal static class TestAssert
+    {
+        public static void True(bool condition, string message)
+        {
+            if (!condition)
+            {
+                throw new System.InvalidOperationException(message);
+            }
+        }
+
+        public static void Equal<T>(T expected, T actual)
+        {
+            if (!object.Equals(expected, actual))
+            {
+                throw new System.InvalidOperationException(
+                    "Expected '" + expected + "' but got '" + actual + "'.");
+            }
+        }
+    }
+}

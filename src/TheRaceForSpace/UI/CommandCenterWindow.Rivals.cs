a84b4e3a71d534353776422dbde099b13c6ccdd9
bdwindowservices-cloud <bdwindowservices@gmail.com>
2026-09-12 18:16:02 +0100

    Hide paid rival research cost

--- src/TheRaceForSpace/UI/CommandCenterWindow.Rivals.cs
+++ src/TheRaceForSpace/UI/CommandCenterWindow.Rivals.cs
@@ -424,7 +424,6 @@ namespace TheRaceForSpace.UI
 
                 GUILayout.BeginVertical("box");
                 GUILayout.Label("Next Research Project: " + displayName, _boldLabelStyle);
-                GUILayout.Label("Science Cost: " + research.ScienceCostPaid.ToString("0.#") + " - Paid");
                 GUILayout.Label("Status: " + researchStatus);
                 GUILayout.Label("Started: " + FormatKerbinDate(research.StartUniversalTime));
                 GUILayout.Label(
